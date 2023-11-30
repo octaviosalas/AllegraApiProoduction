4f149f1a9046ac1e92abf90e69f5d9d17b293bc6
Octavio Salas <salasoctavio129@gmail.com>
2023-11-29 22:34:09 -0300

    intentExcel

--- controllers/orders.controllers.js
+++ controllers/orders.controllers.js
@@ -1,6 +1,9 @@
 import Orders from "../models/orders.js"
 import xlsx from 'xlsx';
+import multer from "multer";
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 export const getAllOrders = async (req, res) => { 
   console.log("Recibi una peticion")
@@ -46,7 +49,34 @@ export const createNewOrder = async (req, res) => {
     
 }
 
-export const EditOrder = async (req, res) => { 
+export const EditOrderState = async (req, res) => {
+  const { orderId } = req.params;
+  const { state } = req.body;
+
+  try {
+    const updatedOrder = await Orders.findByIdAndUpdate(
+      { _id: orderId },
+      {
+        $set: {
+          state: state,
+          'orderDetail.$[].orderState': state,
+        },
+      },
+      { new: true }
+    );
+
+    if (!updatedOrder) {
+      return res.status(404).json({ message: 'Order not found!' });
+    }
+
+    res.status(200).json({ message: 'The Order has been edited successfully', orderEdited: updatedOrder });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
+
+export const EditOrderData = async (req, res) => { 
   const { orderId } = req.params;
   const {state, manufacturingCost} = req.body
 
@@ -70,8 +100,6 @@ export const EditOrder = async (req, res) => {
 
 
 
-
-
 export const DeleteOrder = async (req, res) => { 
   const { orderId } = req.params;
   console.log("RECIBI:", req.params)
@@ -91,37 +119,52 @@ export const DeleteOrder = async (req, res) => {
 }
 
 
-
-
-export const savePublication = async (req, res) => { 
-   
-    const {creatorName, creatorId, publicationDate, publicationImages, publicationTitle, typeOfPublication, creatorLocation, address, creatorProfileImage, publicationDescription, resolved} = req.body
-    console.log(req.body)
-    console.log("RECIBI UNA NUEVA PUBLICACION")
-    
-
-    try {
-       const saveNewPub = new Publications ( { 
-         creatorName: creatorName,
-         creatorId: creatorId,
-         publicationDate: publicationDate,
-         publicationImages: publicationImages,
-         publicationTitle: publicationTitle,
-         typeOfPublication: typeOfPublication,
-         creatorLocation: creatorLocation,
-         address: address,
-         creatorProfileImage: creatorProfileImage,
-         publicationDescription: publicationDescription,
-         resolved: resolved
-       })
-       saveNewPub.save()
-                 .then((saved) => { 
-                  res.json({message: "The Publication was Posted Succesfully!", saved})
-                 })
-                 .catch((err) => { 
-                  console.log(err)
-                 })
-    } catch (error) {
-      console.log(error)
+export const addExcelDocument = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send('No se ha proporcionado un archivo Excel');
     }
-}
+
+    const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+
+    // Obtener el rango de filas
+    const range = xlsx.utils.decode_range(sheet['!ref']);
+    const rows = range.e.r + 1;
+
+    // Recorrer todas las filas
+    for (let i = 1; i < rows; i++) {
+      const manufacturingCost = sheet['A' + (i + 1)].v;
+      const state = sheet['B' + (i + 1)].v;
+      const orderDetailString = sheet['C' + (i + 1)].v;
+
+      // Separar los objetos usando '|||' y '///'
+      const propertiesArray = orderDetailString.split('|||');
+      const orderDetail = propertiesArray.map((objString) => {
+        const objProperties = objString.split('///');
+        return {
+          CodigoProducto: objProperties[0],
+          nombre: objProperties[1],
+          cantidad: parseInt(objProperties[2], 10),
+          observaciones: objProperties[3],
+        };
+      });
+
+      // Crear una nueva instancia del modelo Orders
+      const newOrder = new Orders({
+        manufacturingCost,
+        state,
+        orderDetail,
+      });
+
+      // Guardar en la base de datos
+      await newOrder.save();
+    }
+
+    res.status(200).send('Datos almacenados exitosamente en la base de datos');
+  } catch (error) {
+    console.error('Error al procesar los datos:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+};
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,11 +1,13 @@
 import express from "express"
 const ordersRoutes = express.Router()
-import { getAllOrders, createNewOrder, getOneOrder, EditOrder, DeleteOrder } from "../controllers/orders.controllers.js"
+import { getAllOrders, createNewOrder, getOneOrder, EditOrderState, DeleteOrder, EditOrderData, addExcelDocument } from "../controllers/orders.controllers.js"
 
 ordersRoutes.post("/newOrder", createNewOrder)
+ordersRoutes.post("/addOrderWithExcel", addExcelDocument)
 ordersRoutes.get("/allOrders", getAllOrders)
 ordersRoutes.get("/orderData/:orderId", getOneOrder)
-ordersRoutes.put("/editOrder/:orderId", EditOrder)
+ordersRoutes.put("/editOrderState/:orderId", EditOrderState)
+ordersRoutes.put("/editOrder/:orderId", EditOrderData)
 ordersRoutes.delete("/deleteOrder/:orderId", DeleteOrder)
 
 
